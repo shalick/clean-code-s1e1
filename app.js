27d266d6eff944fbd0f34e01cdbd0e4b38403c0a
Alick Shabanovich <alikshabanovich@gmail.com>
2022-11-03 04:22:09 +0300

    fix: adjust formatting according to 2.1 rule

--- app.js
+++ app.js
@@ -32,19 +32,23 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.className="tasks__item";
     label.innerText=taskString;
-    label.className='task';
+    label.className="tasks__label task";
+
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="tasks__text-input task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit";
 
     deleteButton.className="delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className="tasks__delete-img";
+    deleteButtonImg.alt="remove";
     deleteButton.appendChild(deleteButtonImg);
 
 
